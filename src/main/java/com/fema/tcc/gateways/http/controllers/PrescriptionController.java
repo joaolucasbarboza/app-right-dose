47981e1a6a082e769713e279102e9d05bb445321
joaobarboza <joaolucasbarbozaa@gmail.com>
2025-07-29 19:28:53 -0300

    refactor: update prescription endpoint to use plural path

--- src/main/java/com/fema/tcc/gateways/http/controllers/PrescriptionController.java
+++ src/main/java/com/fema/tcc/gateways/http/controllers/PrescriptionController.java
@@ -18,7 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "prescription")
+@RequestMapping(value = "prescriptions")
 @AllArgsConstructor
 public class PrescriptionController {
 
