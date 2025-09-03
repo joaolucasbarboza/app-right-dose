70a2941dbeeabc67f52b94e305891dbde827cf76
joaobarboza <joaolucasbarbozaa@gmail.com>
2025-09-02 22:43:17 -0300

    edit bean firebaseConfig

--- src/main/java/com/fema/tcc/config/firebase/FirebaseConfig.java
+++ src/main/java/com/fema/tcc/config/firebase/FirebaseConfig.java
@@ -2,28 +2,36 @@ package com.fema.tcc.config.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import org.springframework.beans.factory.annotation.Value;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.FileInputStream;
+import org.springframework.core.io.Resource;
 import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FirebaseConfig {
 
-  @Bean
-  public FirebaseApp firebaseApp() throws IOException {
-    FileInputStream serviceAccount =
-        new FileInputStream("src/main/resources/firebase-service-account.json");
+    @Value("${firebase.service-account-file}")
+    private Resource serviceAccountResource;
 
-    FirebaseOptions options =
-        new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        if (!FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.getInstance();
+        }
 
-    return FirebaseApp.initializeApp(options);
-  }
+        try (InputStream is = serviceAccountResource.getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(is))
+                    .build();
+
+            return FirebaseApp.initializeApp(options);
+        }
+    }
 
   @Bean
   public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
