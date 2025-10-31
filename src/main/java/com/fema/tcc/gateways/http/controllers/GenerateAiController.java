9295f9512dac718402afc6ef552bdefc050b97ed
joaobarboza <joaolucasbarbozaa@gmail.com>
2025-10-31 20:47:47 -0300

    feat: adding log GenerateAiController

--- src/main/java/com/fema/tcc/gateways/http/controllers/GenerateAiController.java
+++ src/main/java/com/fema/tcc/gateways/http/controllers/GenerateAiController.java
@@ -3,11 +3,13 @@ package com.fema.tcc.gateways.http.controllers;
 import com.fema.tcc.gateways.http.jsons.FoodRecommendationResponseJson;
 import com.fema.tcc.usecases.recommendationai.RecommendationService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/generate-ai")
@@ -17,6 +19,8 @@ public class GenerateAiController {
 
   @GetMapping(produces = "application/json;charset=UTF-8")
   public ResponseEntity<FoodRecommendationResponseJson> generateAiMvc() {
+    log.info("Generating AI recommendation");
+
     FoodRecommendationResponseJson body = recommendationService.execute().block();
     return ResponseEntity.ok(body);
   }
